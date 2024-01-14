8045b05be9bcbd1fb76805b3db402b9dd9d79944
faizeraza <faizeraza4us@gmail.com>
2024-01-15 00:32:28 +0530

    "second commit"

--- review_analysis/modelsmaker/modelsdev.py
+++ review_analysis/modelsmaker/modelsdev.py
@@ -1,23 +1,36 @@
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression as lr
+from sklearn.neighbors import KNeighborsClassifier 
 import pandas as pd
 # from sklearn.metrics import accuracy_score as ac
 # from sklearn.metrics import confusion_matrix as cm
 import pickle
 
-def mnbmodeltrain():
-    X_train = pd.read_csv("review_analysis/data/processed/X_train.csv")
-    y_train = pd.read_csv("review_analysis/data/processed/y_train.csv")
+X_train = pd.read_csv("review_analysis/data/processed/X_train.csv")
+y_train = pd.read_csv("review_analysis/data/processed/y_train.csv")
+def mnbmoClassifier():
     print(y_train['feedback'])
     model = MultinomialNB().fit(X_train, y_train['feedback'])
     return model
+def regression():
+    regr = lr()
+    regr = regr.fit(X_train,y_train["feedback"])
+    return regr
 
+def knnClassifier():
+    knn = KNeighborsClassifier(n_neighbors = 5).fit(X_train,y_train["feedback"])
+    return knn
 def saveModel(model,filename):
     pickle.dump(model,open(filename, 'wb'))
 
 if __name__ == "__main__":
-    model = mnbmodeltrain()
+    mnbmodel = mnbmoClassifier()
     modelname = "review_analysis/models/MNBClassifier"
-    saveModel(model,modelname)
+    saveModel(mnbmodel,modelname)
+    rgrmodel = regression()
+    saveModel(rgrmodel,"review_analysis/models/lregression")
+    knn = knnClassifier()
+    saveModel(knn,"review_analysis/models/knnClassifier")
     # X_test = pd.read_csv("review_analysis/data/processed/X_test.csv")
     # y_test = pd.read_csv("review_analysis/data/processed/y_test.csv")
     # filename = "review_analysis/models/MNBClassifier"
